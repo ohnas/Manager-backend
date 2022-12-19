7140c36d7c2e2c20cf5c7af1df6340f3a05c77d7
ohnas <ohnas12@gmail.com>
2022-12-19 17:26:55 +0900

    2. custom user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+
+    list_display = ("username", "name", "email")
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "username",
+                    "password",
+                    "name",
+                    "email",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
-    pass
+
+    first_name = models.CharField(editable=False, max_length=150)
+    last_name = models.CharField(editable=False, max_length=150)
+    name = models.CharField(max_length=150, default="")
