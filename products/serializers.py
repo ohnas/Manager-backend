e6012b957d91496ac117555a72ae0eeecbdceda0
ohnas <ohnas12@gmail.com>
2022-12-31 18:09:01 +0900

    22. ProductSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from products.models import Product
+from brands.serializers import BrandSerializer
 
 
 class ProductSerializer(ModelSerializer):
+
+    brand = BrandSerializer(read_only=True)
+
     class Meta:
         model = Product
         fields = (
+            "brand",
             "name",
             "cost",
         )
