4d2cb3c0302839a0d58063aa74709e4cbb0306bb
ohnas <ohnas12@gmail.com>
2023-01-02 12:40:25 +0900

    30. models.site

--- sites/models.py
+++ sites/models.py
@@ -9,6 +9,8 @@ class Site(CommonModel):
     name = models.CharField(max_length=150)
     url = models.URLField()
     brand = models.ForeignKey(to="brands.Brand", on_delete=models.CASCADE)
+    api_key = models.CharField(max_length=150, default="")
+    secret_key = models.CharField(max_length=150, default="")
 
     def __str__(self) -> str:
         return self.name
