c51d8f315a9597998b83dcd7b40c602664e88735
ohnas <ohnas12@gmail.com>
2023-01-05 14:20:44 +0900

    38. models.Advertising

--- advertisings/models.py
+++ advertisings/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 from common.models import CommonModel
 
-# Create your models here.
-
 
 class Advertising(CommonModel):
-    pass
+
+    site = models.ForeignKey(to="sites.Site", on_delete=models.SET_NULL, null=True)
+    campaign_id = models.CharField(max_length=150)
+    campaign_name = models.CharField(max_length=150)
+    reach = models.CharField(max_length=150)
+    impressions = models.CharField(max_length=150)
+    frequency = models.CharField(max_length=150)
+    spend = models.CharField(max_length=150)
+    cpm = models.CharField(max_length=150)
+    purchase_roas = models.CharField(max_length=150)
+    website_ctr = models.CharField(max_length=150)
+    cost_per_unique_inline_link_click = models.CharField(max_length=150)
+    purchase = models.CharField(max_length=150)
+    landing_page_view = models.CharField(max_length=150)
+    link_click = models.CharField(max_length=150)
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.campaign_name
