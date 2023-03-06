cb880cc94d4b879e3e8c9bbd9d9f603c1624a5cf
ohnas <ohnas12@gmail.com>
2023-03-06 13:38:08 +0900

    88. DB refresh

--- products/models.py
+++ products/models.py
@@ -35,6 +35,7 @@ class Options(CommonModel):
 class Event(CommonModel):
 
     name = models.CharField(max_length=200)
+    brand = models.ForeignKey(to="brands.Brand", on_delete=models.CASCADE)
     product = models.ForeignKey(to="products.Product", on_delete=models.CASCADE)
     event_date = models.DateField()
 
