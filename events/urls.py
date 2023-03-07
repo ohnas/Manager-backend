f83178cbfc698eff3ac7e589025d15514c810eb3
ohnas <ohnas12@gmail.com>
2023-03-07 16:15:37 +0900

    91. events.views CreateEvent

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from events.views import Events
+from events.views import Events, CreateEvent
 
 urlpatterns = [
     path("<int:brand_pk>", Events.as_view()),
+    path("<int:brand_pk>/create", CreateEvent.as_view()),
 ]
--- events/views.py
+++ events/views.py
@@ -1,8 +1,10 @@
+from django.db import transaction
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ParseError
 from brands.models import Brand
+from products.models import Product
 from events.serializers import EventSerializer
 
 # Create your views here.
@@ -25,3 +27,34 @@ class Events(APIView):
         events = brand.event_set.filter(event_date__range=(from_date, to_date))
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
+
+
+class CreateEvent(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, brand_pk):
+        try:
+            return Brand.objects.get(pk=brand_pk)
+        except Brand.DoesNotExist:
+            raise NotFound
+
+    def post(self, request, brand_pk):
+        brand = self.get_object(brand_pk)
+        name = request.data.get("name")
+        product = request.data.get("product")
+        event_date = request.data.get("event_date")
+        if not name or not product or not event_date:
+            raise ParseError
+        product = Product.objects.get(pk=product)
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            with transaction.atomic():
+                event = serializer.save(
+                    brand=brand,
+                    product=product,
+                )
+                serializer = EventSerializer(event)
+                return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
