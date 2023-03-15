b6c1189bcfeefee8abc64013579306ff4fd71cc9
ohnas <ohnas12@gmail.com>
2023-03-15 18:43:23 +0900

    ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Manager-backend-dev.ap-northeast-2.elasticbeanstalk.com"]
 
 # Application definition
 
