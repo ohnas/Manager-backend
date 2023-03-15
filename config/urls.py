06a978632ecaeb349f10ab578db12d4ff8c6cec4
ohnas <ohnas12@gmail.com>
2023-03-15 19:09:11 +0900

    health_chechk

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path("", views.health_check, name="health_check"),
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("users.urls")),
     path("api/v1/brands/", include("brands.urls")),
