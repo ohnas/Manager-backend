7b4d389013f15634138e2f83c9138cb2e7fd360c
ohnas <ohnas12@gmail.com>
2023-05-17 13:51:22 +0900

    brands.models Data

--- brands/admin.py
+++ brands/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from brands.models import Brand
+from brands.models import Brand, Data
 
 # Register your models here.
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Data)
+class DataAdmin(admin.ModelAdmin):
     pass
--- brands/models.py
+++ brands/models.py
@@ -5,10 +5,24 @@ from common.models import CommonModel
 
 
 class Brand(CommonModel):
-
     name = models.CharField(max_length=150, unique=True)
     description = models.TextField(null=True, blank=True)
     user = models.ForeignKey(to="users.User", on_delete=models.SET_NULL, null=True)
 
     def __str__(self) -> str:
         return self.name
+
+
+class Data(CommonModel):
+    brand = models.ForeignKey(to="brands.Brand", on_delete=models.CASCADE)
+    imweb_price = models.IntegerField()
+    imweb_deliv_price = models.IntegerField()
+    product_cost = models.IntegerField()
+    product_profit = models.IntegerField()
+    facebook_ad_expense_krw = models.IntegerField()
+    expense = models.IntegerField()
+    operating_profit = models.IntegerField()
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.brand.name}'s Data from {self.date}"
