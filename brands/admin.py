12ee3c96b65198cd47c6d9b468f478dc9d2874e2
ohnas <ohnas12@gmail.com>
2023-05-22 15:28:33 +0900

    brands.admin ExpenseByHandAdmin

--- brands/admin.py
+++ brands/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from brands.models import Brand, BrandData
+from brands.models import Brand, BrandData, ExpenseByHand
 
 # Register your models here.
 
@@ -11,4 +11,9 @@ class BrandAdmin(admin.ModelAdmin):
 
 @admin.register(BrandData)
 class DataAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ExpenseByHand)
+class ExpenseByHandAdmin(admin.ModelAdmin):
     pass
